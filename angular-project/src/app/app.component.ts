cb7ba78ca0492f1a500c8bf6d9c2e665f06bdd5f
Diego Alencar Jacober <diegoalencar.jacober@gmail.com>
2024-04-29 14:55:23 -0300

    :tada: first commit

---
+++ angular-project/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, RouterModule,],
+  // template: `<h1>Hello world!</h1>`,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
